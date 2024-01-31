719d163eadf1d844d9fb2b5e3a9946fa75a3f75c
douglasochaves <dodopen2019@gmail.com>
2024-01-31 17:58:30 -0300

    Criando Reposítório

---
+++ src/main/java/br/com/brq/brqingresso/BrqIngressoApplication.java
@@ -0,0 +1,13 @@
+package br.com.brq.brqingresso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrqIngressoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrqIngressoApplication.class, args);
+	}
+
+}
